9ac0f1db8362c023444c3d8b1ed54944d3fd09c0
teunbrand <tahvdbrand@gmail.com>
2020-03-05 19:31:58 +0100

    Update for ggplot2 v3.3.0

--- R/utils.R
+++ R/utils.R
@@ -32,12 +32,12 @@ try_require <- function(package, fun) {
     "continuous_range",
     "convertInd",
     "df.grid",
-    # "draw_axis_labels",
-    # "reshape_add_margins",
+    "draw_axis_labels",
+    "reshape_add_margins",
     "new_data_frame",
     "defaults", "id",
     "empty",
-    # "eval_facets",
+    "eval_facets",
     "ggname",
     "rename_aes",
     "mapped_aesthetics",
@@ -52,7 +52,7 @@ try_require <- function(package, fun) {
     "unique_combs",
     "var_list",
     "validate_mapping",
-    # "warn_for_guide_position",
+    "warn_for_guide_position",
     "weave_tables_col",
     "weave_tables_row",
     ".pt"
@@ -65,114 +65,6 @@ try_require <- function(package, fun) {
 
 # Store the needed ggplot internals here
 .int <- .grab_ggplot_internals()
-
-# ggplot v3.30 internals --------------------------------------------------
-
-# Needs to be copied from ggplot2 untill v3.30 is on CRAN, then can be moved to
-# the .grab_ggplot_internals
-
-reshape_add_margins <- function(df, vars, margins = TRUE) {
-  margin_vars <- reshape_margins(vars, margins)
-
-  # Return data frame if no margining necessary
-  if (length(margin_vars) == 0) return(df)
-
-  # Prepare data frame for addition of margins
-  addAll <- function(x) {
-    x <- addNA(x, TRUE)
-    factor(x, levels = c(levels(x), "(all)"), exclude = NULL)
-  }
-  vars <- unique(unlist(margin_vars))
-  df[vars] <- lapply(df[vars], addAll)
-
-  rownames(df) <- NULL
-
-  # Loop through all combinations of margin variables, setting
-  # those variables to (all)
-  margin_dfs <- lapply(margin_vars, function(vars) {
-    df[vars] <- rep(list(factor("(all)")), length(vars))
-    df
-  })
-
-  do.call("rbind", margin_dfs)
-}
-
-reshape_margins <- function(vars, margins = NULL) {
-  if (is.null(margins) || identical(margins, FALSE)) return(NULL)
-
-  all_vars <- unlist(vars)
-  if (isTRUE(margins)) {
-    margins <- all_vars
-  }
-
-  # Start by grouping margins by dimension
-  dims <- lapply(vars, intersect, margins)
-
-  # Next, ensure high-level margins include lower-levels
-  dims <- mapply(function(vars, margin) {
-    lapply(margin, downto, vars)
-  }, vars, dims, SIMPLIFY = FALSE, USE.NAMES = FALSE)
-
-  # Finally, find intersections across all dimensions
-  seq_0 <- function(x) c(0, seq_along(x))
-  indices <- expand.grid(lapply(dims, seq_0), KEEP.OUT.ATTRS = FALSE)
-  # indices <- indices[rowSums(indices) > 0, ]
-
-  lapply(seq_len(nrow(indices)), function(i){
-    unlist(mapply("[", dims, indices[i, ], SIMPLIFY = FALSE))
-  })
-}
-
-.int <- c(.int, list(reshape_add_margins = reshape_add_margins))
-
-eval_facet <- function(facet, data, possible_columns = NULL) {
-  # Treat the case when `facet` is a quosure of a symbol specifically
-  # to issue a friendlier warning
-  if (rlang::quo_is_symbol(facet)) {
-    facet <- as.character(rlang::quo_get_expr(facet))
-
-    if (facet %in% names(data)) {
-      out <- data[[facet]]
-    } else {
-      out <- NULL
-    }
-    return(out)
-  }
-
-  # Key idea: use active bindings so that column names missing in this layer
-  # but present in others raise a custom error
-  env <- rlang::new_environment(data)
-  missing_columns <- setdiff(possible_columns, names(data))
-  undefined_error <- function(e) stop("Unknown error")
-  bindings <- rlang::rep_named(missing_columns, list(undefined_error))
-  rlang::env_bind_active(env, !!!bindings)
-
-  # Create a data mask and install a data pronoun manually (see ?new_data_mask)
-  mask <- rlang::new_data_mask(env)
-  mask$.data <- rlang::as_data_pronoun(mask)
-
-  tryCatch(
-    rlang::eval_tidy(facet, mask),
-    ggplot2_missing_facet_var = function(e) NULL
-  )
-}
-
-.int <- c(.int, list(eval_facet = eval_facet))
-
-eval_facets <- function(facets, data, possible_columns = NULL) {
-  vars <- .int$compact(lapply(facets, .int$eval_facet, data,
-                         possible_columns = possible_columns))
-  .int$new_data_frame(tibble::as_tibble(vars))
-}
-
-.int <- c(.int, list(eval_facets = eval_facets))
-
-upto <- function(a, b) {
-  b[seq_len(match(a, b, nomatch = 0))]
-}
-downto <- function(a, b) {
-  rev(upto(a, rev(b)))
-}
 
 # Orphan functions --------------------------------------------------------
 
